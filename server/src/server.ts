f8d7dbf5ebbecdfd6ff177665f69951b061c6845
invinciblejai <mailjai.vardhan@gmail.com>
2017-06-01 15:54:51 +0530

    initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,375 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Pavel Odvody 2016
+ * Licensed under the Apache-2.0 License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+'use strict';
+import * as path from 'path';
+import * as fs from 'fs';
+import {
+	IPCMessageReader, IPCMessageWriter, createConnection, IConnection,
+	TextDocuments, Diagnostic, InitializeResult, CodeLens, Command, RequestHandler, CodeActionParams
+} from 'vscode-languageserver';
+import { stream_from_string } from './utils';
+import { DependencyCollector, IDependency, PomXmlDependencyCollector } from './collector';
+import { EmptyResultEngine, SecurityEngine, DiagnosticsPipeline, codeActionsMap } from './consumers';
+
+import { EmptyResultEngineSenti, SecurityEngineSenti, DiagnosticsPipelineSenti, codeActionsMapSenti } from './sentiment';
+
+const url = require('url');
+const https = require('https');
+const http = require('http');
+const request = require('request');
+const winston = require('winston');
+
+ winston.level = 'debug';
+ winston.add(winston.transports.File, { filename: 'bayesian.log' });
+ winston.remove(winston.transports.Console);
+ winston.info('Starting Bayesian');
+
+/*
+let log_file = fs.openSync('file_log.log', 'w');
+let _LOG = (data) => {
+    fs.writeFileSync('file_log.log', data + '\n');
+}
+*/
+
+enum EventStream {
+  Invalid,
+  Diagnostics,
+  CodeLens
+};
+
+let connection: IConnection = null;
+/* use stdio for transfer if applicable */
+if (process.argv.indexOf('--stdio') == -1)
+    connection = createConnection(new IPCMessageReader(process), new IPCMessageWriter(process));
+else
+    connection = createConnection()
+
+let documents: TextDocuments = new TextDocuments();
+documents.listen(connection);
+
+let workspaceRoot: string;
+connection.onInitialize((params): InitializeResult => {
+    workspaceRoot = params.rootPath;
+    connection.console.log("yay am in!! onInitialize");
+    return {
+        capabilities: {
+            textDocumentSync: documents.syncKind,
+            codeActionProvider: true
+        }
+    }
+});
+
+interface IFileHandlerCallback {
+    (uri: string, name: string, contents: string): void;
+};
+
+interface IAnalysisFileHandler {
+    matcher:  RegExp;
+    stream: EventStream;
+    callback: IFileHandlerCallback;
+};
+
+interface IAnalysisFiles {
+    handlers: Array<IAnalysisFileHandler>;
+    file_data: Map<string, string>;
+    on(stream: EventStream, matcher: string, cb: IFileHandlerCallback): IAnalysisFiles;
+    run(stream: EventStream, uri: string, file: string, contents: string): any;
+};
+
+class AnalysisFileHandler implements IAnalysisFileHandler {
+    matcher: RegExp;
+    constructor(matcher: string, public stream: EventStream, public callback: IFileHandlerCallback) {
+        this.matcher = new RegExp(matcher);
+    }
+};
+
+class AnalysisFiles implements IAnalysisFiles {
+    handlers: Array<IAnalysisFileHandler>;
+    file_data: Map<string, string>;
+    constructor() {
+        this.handlers = [];
+        this.file_data = new Map<string, string>();
+    }
+    on(stream: EventStream, matcher: string, cb: IFileHandlerCallback): IAnalysisFiles {
+        this.handlers.push(new AnalysisFileHandler(matcher, stream, cb));
+        return this;
+    }
+    run(stream: EventStream, uri: string, file: string, contents: string): any {
+        for (let handler of this.handlers) {
+            if (handler.stream == stream && handler.matcher.test(file)) {
+                return handler.callback(uri, file, contents);
+            }
+        }
+    }
+};
+
+interface IAnalysisLSPServer
+{
+    connection: IConnection;
+    files:      IAnalysisFiles;
+
+    handle_file_event(uri: string, contents: string): void;
+    handle_code_lens_event(uri: string): CodeLens[];
+};
+
+class AnalysisLSPServer implements IAnalysisLSPServer {
+    constructor(public connection: IConnection, public files: IAnalysisFiles) {}
+
+    handle_file_event(uri: string, contents: string): void {
+        let path_name = url.parse(uri).pathname;
+        let file_name = path.basename(path_name);
+
+        this.files.file_data[uri] = contents;
+
+        this.files.run(EventStream.Diagnostics, uri, file_name, contents);
+    }
+
+    handle_code_lens_event(uri: string): CodeLens[] {
+        let path_name = url.parse(uri).pathname;
+        let file_name = path.basename(path_name);
+        let lenses = [];
+        let contents = this.files.file_data[uri];
+        return this.files.run(EventStream.CodeLens, uri, file_name, contents);
+    }
+};
+
+interface IAggregator
+{
+    callback: any;
+    is_ready(): boolean;
+    aggregate(IDependency): void;
+};
+
+class Aggregator implements IAggregator
+{
+    mapping: Map<IDependency, boolean>;
+    diagnostics: Array<Diagnostic>;
+    constructor(items: Array<IDependency>, public callback: any){
+        this.mapping = new Map<IDependency, boolean>();
+        for (let item of items) {
+            this.mapping.set(item, false);
+        }
+    }
+    is_ready(): boolean {
+        let val = true;
+        for (let m of this.mapping.entries()) {
+            val = val && m[1];
+        }
+        return val;
+    }
+    aggregate(dep: IDependency): void {
+        this.mapping.set(dep, true);
+        if (this.is_ready()) {
+            this.callback();
+        }
+    }
+};
+
+class AnalysisConfig
+{
+    server_url:         string;
+    api_token:          string;
+    forbidden_licenses: Array<string>;
+    no_crypto:          boolean;
+    home_dir:           string;
+
+    constructor() {
+        // TODO: this needs to be configurable
+        this.server_url = process.env.RECOMMENDER_API_URL || "api-url-not-available-in-lsp";
+        this.api_token = process.env.RECOMMENDER_API_TOKEN || "token-not-available-in-lsp";
+        this.forbidden_licenses = [];
+        this.no_crypto = false;
+        this.home_dir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+    }
+};
+
+let config: AnalysisConfig = new AnalysisConfig();
+let files: IAnalysisFiles = new AnalysisFiles();
+let server: IAnalysisLSPServer = new AnalysisLSPServer(connection, files);
+let rc_file = path.join(config.home_dir, '.analysis_rc');
+if (fs.existsSync(rc_file)) {
+    let rc = JSON.parse(fs.readFileSync(rc_file, 'utf8'));
+    if ('server' in rc) {
+        config.server_url = `${rc.server}/api/v1`;
+    }
+}
+
+let DiagnosticsEngines = [SecurityEngine];
+
+let DiagnosticsEnginesSenti = [SecurityEngineSenti];
+
+// TODO: in-memory caching only, this needs to be more robust
+let metadataCache = new Map();
+
+let get_metadata = (ecosystem, name, version, cb) => {
+    let cacheKey = ecosystem + " " + name + " " + version;
+    let metadata = metadataCache[cacheKey];
+    if (metadata != null) {
+        winston.info('cache hit for ' + cacheKey);
+        cb(metadata);
+        return
+    }
+    let part = [ecosystem, name, version].join('/');
+
+    //connection.console.log("serverurl 1"+ config.server_url);
+    //connection.console.log("serverurl 1"+ config.api_token);
+    const options = url.parse(config.server_url);
+    options['path'] += `/component-analyses/${part}/`;
+    options['headers'] = {'Authorization': 'Bearer ' + config.api_token};
+    winston.debug('get ' + options['host'] + options['path']);
+    connection.console.log('get ' + options['host'] + options['path']);
+    https.get(options, function(res){
+        let body = '';
+        res.on('data', function(chunk) { body += chunk; });
+        res.on('end', function(){
+            winston.info('status ' + this.statusCode);
+            if (this.statusCode == 200 || this.statusCode == 202) {
+                let response = JSON.parse(body);
+                winston.debug('response ' + response);
+                metadataCache[cacheKey] = response;
+                connection.console.log("serverurl 2"+ response);
+                cb(response);
+            } else {
+                connection.console.log("serverurl 3");
+                cb(null);
+            }
+        });
+    });
+
+    //make_sentiments_call(name, cacheKey);
+    // connection.console.log("in sentiment call");
+
+    // let options1 = "http://sentiment-http-sentiment-score.dev.rdu2c.fabric8.io/api/v1.0";
+    // options1 += "/getsentimentanalysis?package="+name;
+    //options['headers'] = {'Authorization': 'Bearer ' + config.api_token};
+    //winston.debug('get sentiment' + options1['host'] + options1['path']);
+    
+};
+
+let sentiment_api_call = (ecosystem, name, version, cb) =>{
+    connection.console.log('get sentiment');
+
+    http.get("http://sentiment-http-sentiment-score.dev.rdu2c.fabric8.io/api/v1.0/getsentimentanalysis/?package="+name, function(res){
+        let body = '';
+        connection.console.log("Got res: " +res);
+        res.on('data', function(chunk) { 
+            body += chunk; 
+            connection.console.log("Got chunk: " +chunk);
+        });
+        res.on('end', function(){
+            winston.info('status ' + this.statusCode);
+            connection.console.log("serverurl sentiment status" + this.statusCode);
+            if (this.statusCode == 200 || this.statusCode == 202) {
+                let response = JSON.parse(body);
+                winston.debug('response ' + response);
+                //metadataCache[cacheKey] = response;
+                connection.console.log("serverurl sentiment 2"+ response);
+                cb(response);
+            } else {
+                connection.console.log("serverurl sentiment 3");
+                cb(null);
+            }
+        });
+    }).on('error', function(e) {
+        connection.console.log("Got error: " + e.message);
+    });
+}
+
+
+files.on(EventStream.Diagnostics, "^package\\.json$", (uri, name, contents) => {
+    /* Convert from readable stream into string */
+    let stream = stream_from_string(contents);
+    let collector = new DependencyCollector(null);
+
+    collector.collect(stream).then((deps) => {
+        let diagnostics = [];
+        /* Aggregate asynchronous requests and send the diagnostics at once */
+        let aggregator = new Aggregator(deps, () => {
+            connection.sendDiagnostics({uri: uri, diagnostics: diagnostics});
+        });
+        for (let dependency of deps) {
+            get_metadata('npm', dependency.name.value, dependency.version.value, (response) => {
+                if (response != null) {
+                    let pipeline = new DiagnosticsPipeline(DiagnosticsEngines, dependency, config, diagnostics);
+                    pipeline.run(response);
+                }
+                aggregator.aggregate(dependency);
+            });
+            //TODO :: sentiment analysis
+            sentiment_api_call('npm', dependency.name.value, dependency.version.value, (response) => {
+                if (response != null) {
+                    let pipeline = new DiagnosticsPipelineSenti(DiagnosticsEnginesSenti, dependency, config, diagnostics);
+                    pipeline.run(response);
+                }
+                aggregator.aggregate(dependency);
+            });
+
+        }
+    });
+});
+
+files.on(EventStream.Diagnostics, "^pom\\.xml$", (uri, name, contents) => {
+    /* Convert from readable stream into string */
+    let stream = stream_from_string(contents);
+    let collector = new PomXmlDependencyCollector();
+
+    collector.collect(stream).then((deps) => {
+        let diagnostics = [];
+        /* Aggregate asynchronous requests and send the diagnostics at once */
+        let aggregator = new Aggregator(deps, () => {
+            connection.sendDiagnostics({uri: uri, diagnostics: diagnostics});
+        });
+        for (let dependency of deps) {
+            get_metadata('maven', dependency.name.value, dependency.version.value, (response) => {
+                if (response != null) {
+                    let pipeline = new DiagnosticsPipeline(DiagnosticsEngines, dependency, config, diagnostics);
+                    pipeline.run(response);
+                }
+                aggregator.aggregate(dependency);
+            });
+        }
+    });
+});
+
+let checkDelay;
+connection.onDidSaveTextDocument((params) => {
+    clearTimeout(checkDelay);
+    server.handle_file_event(params.textDocument.uri, server.files.file_data[params.textDocument.uri]);
+});
+
+connection.onDidChangeTextDocument((params) => {
+    /* Update internal state for code lenses */
+    //connection.console.log('It is here in onDidChange' + server.files.file_data[params.textDocument.uri]);
+    server.files.file_data[params.textDocument.uri] = params.contentChanges[0].text;
+    server.handle_file_event(params.textDocument.uri, server.files.file_data[params.textDocument.uri])
+    clearTimeout(checkDelay);
+    checkDelay = setTimeout(() => {
+        connection.console.log('Inside onDidChange delay');
+        server.handle_file_event(params.textDocument.uri, server.files.file_data[params.textDocument.uri])
+    }, 500)
+});
+
+connection.onDidOpenTextDocument((params) => {
+    connection.console.log("didOpenDoc");
+    server.handle_file_event(params.textDocument.uri, params.textDocument.text);
+});
+
+connection.onCodeAction((params, token): Command[] => {
+    clearTimeout(checkDelay);
+    let commands: Command[] = [];
+    for (let diagnostic of params.context.diagnostics) {
+        let command = codeActionsMap[diagnostic.message];
+        if (command != null) {
+            commands.push(command)
+        }
+    }
+    return commands
+});
+
+connection.onDidCloseTextDocument((params) => {
+    clearTimeout(checkDelay);
+});
+
+connection.listen();
